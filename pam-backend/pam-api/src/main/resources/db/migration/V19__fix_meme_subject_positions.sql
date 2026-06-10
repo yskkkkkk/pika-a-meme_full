b3610f677c48235434f0f7da20ffb66456abc2d0
STO1709 <fb1014@sk.com>
2026-06-10 15:23:46 +0900

    fix: 신규 밈 이미지(V18) 5종의 subject_position 렌더링 오류 수정 (V19)

---
+++ pam-backend/pam-api/src/main/resources/db/migration/V19__fix_meme_subject_positions.sql
@@ -0,0 +1,28 @@
+﻿-- ============================================================
+-- V19: V18에서 잘못 들어간 subject_position 5건 수정
+-- ============================================================
+
+-- 03. 물고기_고양이 (중앙, 양옆 비어있음) -> FULL_VERTICAL
+UPDATE meme_images 
+SET subject_position = 'FULL_VERTICAL' 
+WHERE image_url LIKE '%fish_cat.png%';
+
+-- 04. 행복_동물들 (중앙, 상하단 비어있음) -> FULL_HORIZONTAL
+UPDATE meme_images 
+SET subject_position = 'FULL_HORIZONTAL' 
+WHERE image_url LIKE '%happy_animals.png%';
+
+-- 05. 부자_강아지 (중앙, 양옆 비어있음) -> FULL_VERTICAL
+UPDATE meme_images 
+SET subject_position = 'FULL_VERTICAL' 
+WHERE image_url LIKE '%rich_dog.png%';
+
+-- 06. 호기심_강아지 (중앙, 양옆 비어있음) -> FULL_VERTICAL
+UPDATE meme_images 
+SET subject_position = 'FULL_VERTICAL' 
+WHERE image_url LIKE '%curious_dog.png%';
+
+-- 08. 어이없어하는_강아지 (중앙, 상하단 비어있음) -> FULL_HORIZONTAL
+UPDATE meme_images 
+SET subject_position = 'FULL_HORIZONTAL' 
+WHERE image_url LIKE '%dumbfounded_dog.png%';
